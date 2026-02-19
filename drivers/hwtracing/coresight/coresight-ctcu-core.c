396cbf5a4e3036be016ebc0a01aa1391d850f342
Jie Gan <jie.gan@oss.qualcomm.com>
2026-02-19 22:46:57 +0800

    FROMLIST: coresight: ctcu: fix the spin_bug
    Acquiring an uninitialized raw_spin_lock is invalid and may trigger
    unexpected behavior or spin_bug.
    
    Link: https://lore.kernel.org/all/20260219-fix-spin-lock-issue-v1-1-557f7d513d7e@oss.qualcomm.com/
    Fixes: f78d206f3d73 ("Coresight: Add Coresight TMC Control Unit driver")
    Reviewed-by: James Clark <james.clark@linaro.org>
    Signed-off-by: Jie Gan <jie.gan@oss.qualcomm.com>

--- drivers/hwtracing/coresight/coresight-ctcu-core.c
+++ drivers/hwtracing/coresight/coresight-ctcu-core.c
@@ -306,6 +306,7 @@ static int ctcu_probe(struct platform_device *pdev)
 	desc.groups = ctcu_attr_grps;
 	desc.ops = &ctcu_ops;
 	desc.access = CSDEV_ACCESS_IOMEM(base);
+	raw_spin_lock_init(&drvdata->spin_lock);
 
 	drvdata->csdev = coresight_register(&desc);
 	if (IS_ERR(drvdata->csdev))
