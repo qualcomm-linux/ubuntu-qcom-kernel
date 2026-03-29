e93a08c6b7626f4a5ecee2cec5f40dccc27782c7
Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
2026-03-29 02:33:10 +0200

    FROMGIT: media: qcom: iris: split platform data from firmware data
    Finalize the logical separation of the software and hardware interface
    descriptions by moving hardware properties to the files specific to the
    particular VPU version.
    
    Link: https://lore.kernel.org/linux-media/20260329-iris-platform-data-v11-9-eea672b03a95@oss.qualcomm.com/
    Reviewed-by: Dikshita Agarwal <dikshita.agarwal@oss.qualcomm.com>
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
    Signed-off-by: Bryan O'Donoghue <bod@kernel.org>
    (cherry picked from commit 5f21ce799f4c3e98109b85dc024bb69f938d077a)
    (cherry picked from commit 7b32dbd8323317f67a2466a4b63c57e8258c3705)

--- drivers/media/platform/qcom/iris/iris_platform_vpu3x.c
+++ drivers/media/platform/qcom/iris/iris_platform_vpu3x.c
@@ -71,85 +71,6 @@ static const struct tz_cp_config tz_cp_config_vpu3[] = {
  * - inst_caps to platform_inst_cap_qcs8300
  */
 const struct iris_platform_data qcs8300_data = {
-	.firmware_desc_gen2 = &iris_vpu30_p4_s6_gen2_desc,
-	.vpu_ops = &iris_vpu3_ops,
-	.icc_tbl = iris_icc_info_vpu3x,
-	.icc_tbl_size = ARRAY_SIZE(iris_icc_info_vpu3x),
-	.clk_rst_tbl = sm8550_clk_reset_table,
-	.clk_rst_tbl_size = ARRAY_SIZE(sm8550_clk_reset_table),
-	.bw_tbl_dec = iris_bw_table_dec_vpu3x,
-	.bw_tbl_dec_size = ARRAY_SIZE(iris_bw_table_dec_vpu3x),
-	.pmdomain_tbl = &iris_pmdomain_table_vpu3x,
-	.opp_pd_tbl = iris_opp_pd_table_vpu3x,
-	.opp_pd_tbl_size = ARRAY_SIZE(iris_opp_pd_table_vpu3x),
-	.clk_tbl = sm8550_clk_table,
-	.clk_tbl_size = ARRAY_SIZE(sm8550_clk_table),
-	.opp_clk_tbl = iris_opp_clk_table_vpu3x,
-	/* Upper bound of DMA address range */
-	.dma_mask = 0xe0000000 - 1,
-	.inst_iris_fmts = iris_fmts_vpu3x_dec,
-	.inst_iris_fmts_size = ARRAY_SIZE(iris_fmts_vpu3x_dec),
-	.inst_caps = &platform_inst_cap_qcs8300,
-	.tz_cp_config_data = tz_cp_config_vpu3,
-	.tz_cp_config_data_size = ARRAY_SIZE(tz_cp_config_vpu3),
-	.num_vpp_pipe = 2,
-	.max_session_count = 16,
-	.max_core_mbpf = ((4096 * 2176) / 256) * 4,
-	.max_core_mbps = (((3840 * 2176) / 256) * 120),
-};
-
-static int sm8550_init_cb_devs(struct iris_core *core)
-{
-	const u32 f_id_np = 0; /* IRIS_NON_PIXEL_VCODEC */
-	const u32 f_id_p = 1;  /* IRIS_PIXEL */
-	struct device *dev;
-
-	dev = iris_create_cb_dev(core, "iris_non_pixel", &f_id_np);
-	if (IS_ERR(dev))
-		return PTR_ERR(dev);
-
-	core->dev_np = dev;
-	core->dev_bs = core->dev_np;
-
-	dev = iris_create_cb_dev(core, "iris_pixel", &f_id_p);
-	if (IS_ERR(dev))
-		goto err_unreg_dev_np;
-
-	core->dev_p = dev;
-
-	return 0;
-
-err_unreg_dev_np:
-	platform_device_unregister(to_platform_device(core->dev_np));
-	core->dev_np = NULL;
-	core->dev_bs = NULL;
-
-	return PTR_ERR(dev);
-}
-
-const struct iris_firmware_desc iris_vpu36_p4_s7_gen2_desc = {
-	.firmware_data = &iris_hfi_gen2_data,
-	.get_vpu_buffer_size = iris_vpu_buf_size,
-	.fwname = "qcom/vpu/vpu36_p4_s7.mbn",
-};
-
-static void sm8550_deinit_cb_devs(struct iris_core *core)
-{
-	if (core->dev_np)
-		platform_device_unregister(to_platform_device(core->dev_np));
-	if (core->dev_p)
-		platform_device_unregister(to_platform_device(core->dev_p));
-
-	core->dev_np = NULL;
-	core->dev_bs = NULL;
-	core->dev_p = NULL;
-}
-
-/*
- * Shares most of SM8550 data except:
- * - inst_caps to platform_inst_cap_qcs8300
- */
-const struct iris_platform_data qcs8300_data = {
 	.firmware_data = &iris_hfi_gen2_data,
 	.get_vpu_buffer_size = iris_vpu_buf_size,
 	.vpu_ops = &iris_vpu3_ops,
@@ -174,7 +95,6 @@ const struct iris_platform_data qcs8300_data = {
 	.inst_caps = &platform_inst_cap_qcs8300,
 	.tz_cp_config_data = tz_cp_config_vpu3,
 	.tz_cp_config_data_size = ARRAY_SIZE(tz_cp_config_vpu3),
-	.ubwc_config = &iris_ubwc_config_vpu3x,
 	.num_vpp_pipe = 2,
 	.max_session_count = 16,
 	.max_core_mbpf = ((4096 * 2176) / 256) * 4,
@@ -185,8 +105,6 @@ const struct iris_platform_data sm8550_data = {
 	.firmware_data = &iris_hfi_gen2_data,
 	.get_vpu_buffer_size = iris_vpu_buf_size,
 	.vpu_ops = &iris_vpu3_ops,
-	.init_cb_devs = sm8550_init_cb_devs,
-	.deinit_cb_devs = sm8550_deinit_cb_devs,
 	.icc_tbl = iris_icc_info_vpu3x,
 	.icc_tbl_size = ARRAY_SIZE(iris_icc_info_vpu3x),
 	.clk_rst_tbl = sm8550_clk_reset_table,
