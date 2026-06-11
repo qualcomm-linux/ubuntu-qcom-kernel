fa377278d151bce40c7d1509880cd5e0dda83a53
Mohd Ayaan Anwar <mohd.anwar@oss.qualcomm.com>
2026-06-12 00:06:58 +0530

    RFC: net: stmmac: qcom-ethqos: convert ethqos_rgmii_macro_init() to void
    The return value is never checked by its sole caller and the speed
    validation duplicates a check higher up the call stack.  Convert to
    void and remove the dead code.
    
    Signed-off-by: Mohd Ayaan Anwar <mohd.anwar@oss.qualcomm.com>
    Link: https://lore.kernel.org/r/20260612-shikra_ethernet-v1-2-f0f4a1d19929@oss.qualcomm.com

--- drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-ethqos.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-ethqos.c
@@ -373,7 +373,7 @@ static int ethqos_dll_configure(struct qcom_ethqos *ethqos)
 	return 0;
 }
 
-static int ethqos_rgmii_macro_init(struct qcom_ethqos *ethqos, int speed)
+static void ethqos_rgmii_macro_init(struct qcom_ethqos *ethqos, int speed)
 {
 	struct device *dev = &ethqos->pdev->dev;
 	int phase_shift;
@@ -509,10 +509,8 @@ static int ethqos_rgmii_macro_init(struct qcom_ethqos *ethqos, int speed)
 		break;
 	default:
 		dev_err(dev, "Invalid speed %d\n", speed);
-		return -EINVAL;
+		return;
 	}
-
-	return 0;
 }
 
 static int ethqos_configure_rgmii(struct qcom_ethqos *ethqos, int speed)
