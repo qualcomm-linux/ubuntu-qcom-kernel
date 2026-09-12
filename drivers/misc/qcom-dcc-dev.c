d59b554560a561612d188c735350bc562c2c0059
Shiraz Hashim <shiraz.hashim@oss.qualcomm.com>
2026-09-12 12:37:34 +0530

    Revert "QCLINUX: qcom-dcc: add shikra register list and move config to header"
    This reverts commit 266565770ded7459cf1e81d068bb2be8dafb19e9.
    
    Shikra platform support in this tree is being retired in favor of the
    latest community Shikra patches; drop this downstream commit to avoid
    conflicting with the upstream series.
    
    Signed-off-by: Shiraz Hashim <shiraz.hashim@oss.qualcomm.com>

--- drivers/misc/qcom-dcc-dev.c
+++ drivers/misc/qcom-dcc-dev.c
@@ -11,7 +11,6 @@
 #include "qcom-dcc-lemans-config.h"
 #include "qcom-dcc-kodiak-config.h"
 #include "qcom-dcc-pakala-config.h"
-#include "qcom-dcc-shikra-config.h"
 
 #define DEV_NAME "qcom-dcc"
 
@@ -131,13 +130,6 @@ static int __init dcc_dev_init(void)
 		if (ret)
 			goto fail;
 
-		break;
-	case 756:
-	case 758:
-	case 759:
-		ret = platform_device_add_data(dcc_pdev, &shikra_pdata, sizeof(shikra_pdata));
-		if (ret)
-			goto fail;
 		break;
 	default:
 		pr_err("DCC: Invalid SoC ID\n");
